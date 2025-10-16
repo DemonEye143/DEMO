12ff8e4d8c5815901ca2a5544c97ec693ebfa6f1
𝐋𝐮𝐜𝐢𝐟𝐞𝐫....!!🌈™ <scamerme22@gmail.com>
2025-10-16 08:02:32 +0530

    𝐋𝐮𝐜𝐢𝐟𝐞𝐫....!!🌈™

---
+++ settings.js
@@ -0,0 +1,15 @@
+const settings = {
+  packname: 'Knight Bot',
+  author: '‎',
+  botName: "𝐍𝐈𝐓𝐑𝐎 𝐗 𝐁𝐎𝐓....!!🌈™ ⚡",
+  botOwner: '𝐋𝐮𝐜𝐢𝐟𝐞𝐫....!!',
+  ownerNumber: '919339344963',
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "👑 𝙎𝙃𝘼𝙍𝙀 𝘼𝙉𝘿 ⚡️𝙎𝙐𝙋𝙋𝙊𝙍𝙏 ❤️ 𝐃𝐄𝐌𝐎𝐍 𝐄𝐘𝐄....!! 😈",
+  version: "2.2.2",
+};
+
+module.exports = settings;
